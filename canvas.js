f9cc8a7ef7186a48b27929ac8b1df1d70104d55e
unknown <lolry0207>
2021-12-29 10:52:09 +0900

    final

--- canvas.js
+++ canvas.js
@@ -1,13 +1,13 @@
 const canvas = document.querySelector('canvas')
-const c  = canvas.getContext('2d') // 2d 게임, canvas 객체의 api 속성에 넣어주는 것
+const c  = canvas.getContext('2d') // 2d 게임
 
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-const scoreEl = document.querySelector('#scoreEl')
-const startBtn = document.querySelector('#startBtn')
-const modalEl = document.querySelector('#modalEl')
-const bigScoreEl = document.querySelector('#bigScoreEl')
+const scoreEl = document.querySelector('#scoreEl')          // 게임 내 점수 
+const startBtn = document.querySelector('#startBtn')        // 시작 버튼
+const modalEl = document.querySelector('#modalEl')          // 게임 시작시 작은 창
+const bigScoreEl = document.querySelector('#bigScoreEl')    // 마지막 최종 점수
 
 // 플레이어
 class Player {
@@ -20,8 +20,7 @@ class Player {
 
     draw() { 
         c.beginPath()
-        c.arc(this.x , this.y, this.radius, 0, Math.PI*2, false);
-        // 원을 그릴 수 있게 하는 메서드 | x 좌표, y 좌표, 반지름, 시작 각도, 끝 각도(파이*2 = 360도) , 방향 설정
+        c.arc(this.x , this.y, this.radius, 0, Math.PI*2, false); // 원을 그릴 수 있게 하는 메서드 | x 좌표, y 좌표, 반지름, 시작 각도, 끝 각도(파이*2 = 360도) , 방향 설정
         c.fillStyle = this.color
         c.fill()
     }
@@ -34,12 +33,12 @@ class Projectile {
         this.y = y
         this.radius = radius
         this.color = color
-        this.velocity = velocity
+        this.velocity = velocity    // 속도
     }
 
     draw() { 
         c.beginPath()
-        c.arc(this.x , this.y, this.radius, 0, Math.PI*2, false) // 원을 그릴 수 있게 하는 메서드 | x 좌표, y 좌표, 반지름, 시작 각도, 끝 각도(파이*2 = 360도) , 방향 설정
+        c.arc(this.x , this.y, this.radius, 0, Math.PI*2, false)
         c.fillStyle = this.color
         c.fill()
     }
@@ -75,15 +74,16 @@ class Enemy {
     }
 }
 
+// 화면의 중앙에 플레이어가 위치하도록
 const x = canvas.width / 2
 const y = canvas.height / 2
-// 화면의 중앙에 플레이어가 위치하도록
 
-let player = new Player(x, y, 10, 'white') // 플레이어 객체 생성 
+let player = new Player(x, y, 10, 'white') // 플레이어 생성 
 let projectiles = []
 let enemies = []
 
-function init(){ // 게임 시작시 기존 플레이어 객체 초기화
+// 게임 시작시 초기화 함수
+function init(){
     player = new Player(x, y, 10, 'white')
     projectiles = []
     enemies = []
@@ -94,18 +94,19 @@ function init(){ // 게임 시작시 기존 플레이어 객체 초기화
 
 function spawnEnemies(){
     setInterval(() => {
-        const radius = Math.random() * (30 - 4) + 4 // 4 ~ 30 사이 값
+        const radius = Math.random() * (30 - 4) + 4 // 4 ~ 30 사이 값의 크기를 가지는 적 생성
          
         let x
         let y
 
         if(Math.random() < 0.5){
-            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius // 적 랜덤 생성
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
             y = Math.random() * canvas.height
         } else{
             x = Math.random() * canvas.width
             y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
         }
+
         const color = `hsl(${Math.random() * 360}, 50%, 50%)`
 
         const angle = Math.atan2(
@@ -119,7 +120,7 @@ function spawnEnemies(){
         }
 
         enemies.push(new Enemy(x, y, radius, color, velocity))
-    }, 1000)
+    }, 1500) // 1500ms 즉 1.5초 간격으로 생성
 }
 
 let animateId
@@ -154,7 +155,7 @@ function animate(){
         projectiles.forEach((projectile, projectileIndex) => {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
             
-            // 사물이 닿았을 때 // 두 거리가 1보다 작을 때 
+            // 두 거리가 1보다 작을 때 
             if(dist - enemy.radius - projectile.radius < 1){
                 if(enemy.radius - 7 > 3){
                     // 점수 올리기
@@ -167,7 +168,6 @@ function animate(){
                 } else{
                         score += 250
                         scoreEl.innerHTML = score
-
                     setTimeout(() => {
                         enemies.splice(index, 1) 
                         projectiles.splice(projectileIndex, 1)
